2ac2c3bc3d041e49620f7badd4b5f4f79b7bfef7
Clara Lancelle <lancelle.clara@hotmail.com>
2022-11-21 12:16:50 +0100

    add all files

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+//Exo 1
+
+let message = 'Hello World';
+
+console.log(message);
+
+//Exo 2
+
+let firstPart = 'Un matin, un lapin';
+
+let lastPart = ' a tué un chasseur !';
+
+console.log(firstPart + lastPart);
+
+//Exo 3
+
+let firstname = 'Jean';
+
+let age = 30;
+
+let city = 'Paris';
+
+console.log('Bonjour, je m\'appelle '+ firstname + ', j\'ai ' + age + ' ans et j\'habite '+ city );
+
+
+//Exo 4 
+
+let num = 43;
+
+let txt = '7';
+
+let sum = num + Number(txt);
+
+console.log('la somme de '+num+' et '+txt+' est '+sum);
